735ae2a9674aef3ee10391cc75c5b9b0e55eaf02
beduffy <benfduffy@gmail.com>
2023-09-04 15:17:23 +0200

    Too much memory to save 30k images, read 1 by 1 and rewrite to video and renamed main to record_images.py

--- create_video_from_image_folder.py
+++ create_video_from_image_folder.py
@@ -7,19 +7,27 @@ import numpy as np
 def create_night_video_from_folder(folder_path):
     all_filepaths = sorted(glob.glob(folder_path + '*'))
 
-    print('num files before filtering for robot id:', len(all_filepaths))
+    print('num images:', len(all_filepaths))
 
     if len(all_filepaths) > 0:
         img_array = []
-        print('num files:', len(all_filepaths))
         print(all_filepaths[:5])
+
+        img = cv2.imread(all_filepaths[0])
+        height, width, layers = img.shape
+        size = (width, height)
+
+        fn = 'night_video.mp4'
+        _fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+        out = cv2.VideoWriter(fn, _fourcc, 3, size)
 
         for fp in all_filepaths:
             img = cv2.imread(fp)
             height, width, layers = img.shape
-            size = (width,height)
+            size = (width, height)
             
             filename = fp.split('/')[-1]
+            print(filename)
             # filename_date_alone = filename.split('-')
             filename_date_plus = filename.split('img_dt_')[-1]
             filename_date_plus = filename_date_plus.split('_frame')[0]
@@ -28,25 +36,25 @@ def create_night_video_from_folder(folder_path):
 
             cv2.putText(img, text_to_show, (10, 23), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            img_array.append((filename, img))
+            # img_array.append((filename, img))
+            out.write(img)
+        out.release()
         
-        print('Num images: {}'.format(len(img_array)))
+        # print('Num images: {}'.format(len(img_array)))
 
-        img_array = sorted(img_array, key=lambda x: x[0])
+        # img_array = sorted(img_array, key=lambda x: x[0])
         
         # print('Changing directory')
         # os.chdir(snapshots_folder)
         # print('os.getcwd(): ', os.getcwd())
         # fn = '{}_{}_night_video.mp4'.format()
-        fn = 'night_video.mp4'
-        _fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
-        out = cv2.VideoWriter(fn, _fourcc, 3, size)
+        
         
         msg = 'Created night report video at: {}'.format(fn)
         print(msg)
-        for i in range(len(img_array)):
-            out.write(img_array[i][1])
-        out.release()
+        # for i in range(len(img_array)):
+        #     out.write(img_array[i][1])
+        # out.release()
         print('Finished')
     else:
         print('No images recorded, not creating video')
